cd189ededdfb38945e14b953cb033b73343cb6eb
Mishari Al <s1099030@monmouth.edu>
2021-04-22 23:40:47 -0400

    changed log in style

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,22 +1,28 @@
 @extends('layouts.app')
-
+<br> 
+<br>
+<br>
+<br>
+<br>
 @section('content')
 <div class="container">
     <div class="row justify-content-center" >
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+            
+                
 
-                <div class="card-body">
+                
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
                         </div>
                     @endif
+                    <div class="alert alert-success" role="alert">
 
                     {{ __('You are logged in!') }}
-                </div>
-            </div>
+                    </div>
+                
+            
         </div>
     </div>
 </div>
